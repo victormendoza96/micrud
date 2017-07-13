ce9af7a737c653d5372d019ef23ac850047ff76a
victor <vemb96@gmail.com>
2017-07-13 10:21:23 -0400

    second commit

---
+++ app/controllers/crud_controller.rb
@@ -0,0 +1,2 @@
+class CrudController < ApplicationController
+end
---
+++ config/webpack/loaders/sass.js
@@ -0,0 +1,15 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const { env } = require('../configuration.js')
+
+module.exports = {
+  test: /\.(scss|sass|css)$/i,
+  use: ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    use: [
+      { loader: 'css-loader', options: { modules: true, localIdentName: '[name]___[hash:base64:5]', minimize: env.NODE_ENV === 'production' } },
+      { loader: 'postcss-loader', options: { sourceMap: true } },
+      'resolve-url-loader',
+      { loader: 'sass-loader', options: { sourceMap: true } }
+    ]
+  })
+}
